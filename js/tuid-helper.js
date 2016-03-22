182f43bc6982a3085369cb4c1a9acc8017341e7b
Jigar Patel <jigar.patel@clearcapital.com>
2016-03-22 11:26:57 -0700

    Also parsing Server ID & Counter

--- js/tuid-helper.js
+++ js/tuid-helper.js
@@ -8,6 +8,9 @@ function disectTuid(tuid){
     parsedTuid.tuidBase = (parsedTuid.millis * 262144).toString();
     parsedTuid.localDate = new Date(parsedTuid.millis);
     parsedTuid.utcDate = parsedTuid.localDate.toUTCString();
+    var otherParts = tuid.substring(tuid.legnth-6, 6);
+    parsedTuid.serverID = (otherParts & 0xFF).toString();
+    parsedTuid.counter = ((otherParts & 0x3FF00) >> 8).toString();
     return parsedTuid;
     //((milliseconds * 262144) * 1000).toString()
 }
@@ -33,6 +36,8 @@ document.getElementById("disectTuid").onclick = function(){
         document.getElementById("tuidCol").innerHTML = parsedTuid.tuid;
         document.getElementById("millisCol").innerHTML = parsedTuid.millis.toString();
         document.getElementById("tuidBaseCol").innerHTML = parsedTuid.tuidBase.toString();
+        document.getElementById("counterCol").innerHTML = parsedTuid.counter.toString();
+        document.getElementById("serveridCol").innerHTML = parsedTuid.serverID.toString();
         document.getElementById("localDateCol").innerHTML = parsedTuid.localDate.toString();
         document.getElementById("utcDateCol").innerHTML = parsedTuid.utcDate.toString();
     }else{
