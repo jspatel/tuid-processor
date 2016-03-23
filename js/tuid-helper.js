53fd5edebe15390967046634a04e72f8f1fbb31d
Jigar Patel <jigar.patel@clearcapital.com>
2016-03-22 20:31:04 -0700

    Now something is broken

--- js/tuid-helper.js
+++ js/tuid-helper.js
@@ -4,37 +4,79 @@
 function disectTuid(tuid){
     var parsedTuid = {};
     parsedTuid.tuid = tuid;
-    parsedTuid.millis = Math.floor(tuid / 262144);
+    parsedTuid.millis = Math.floor(tuid / 262144).toString();
     parsedTuid.tuidBase = (parsedTuid.millis * 262144).toString();
     parsedTuid.localDate = new Date(parsedTuid.millis);
     parsedTuid.utcDate = parsedTuid.localDate.toUTCString();
     parsedTuid.otherParts = tuid.substr(tuid.length-6);
+    parsedTuid.otherParts = (parsedTuid.otherParts & 0x3FFFF).toString();
     parsedTuid.serverID = (parsedTuid.otherParts & 0xFF).toString();
-    parsedTuid.counter = ((parsedTuid.otherParts & 0x3FF00) >> 8).toString();
+    parsedTuid.counter = (parsedTuid.otherParts >>> 8).toString();
+    parsedTuid.counter = (parsedTuid.counter & 0x3FF).toString();
     return parsedTuid;
     //((milliseconds * 262144) * 1000).toString()
 }
+function bitwiseAnd(a, b){
+    var w = 4294967296; // 2^32
+    var aHI = a / w;
+    var aLO = a % w;
+    var bHI = b / w;
+    var bLO = b % w;
+    var result = ((aHI & bHI) * w + (aLO & bLO)).toString();
+    console.log(result);
+    return result;
+}
+function bitwiseOr(a, b){
+    var w = 4294967296; // 2^32
+    var aHI = a / w;
+    var aLO = a % w;
+    var bHI = b / w;
+    var bLO = b % w;
+    var result = ((aHI | bHI) * w + (aLO | bLO)).toString();
+    console.log(result);
+    return result;
+}
 function getMillis(inputDate, inputTime){
-    var digitsOnly = /^\d{8}$/g;
+    var validateDate = /^\d{8}$/g;
+    var validateTime = /^\d{6}$/g;
     var date = null;
     var month = null;
     var year = null;
     var currentDate = new Date();
-    console.log(inputDate);
-    if(digitsOnly.test(inputDate)){
+    if(validateDate.test(inputDate)){
         currentDate.setFullYear(inputDate.substr(0,4));
         currentDate.setMonth(inputDate.substr(4,2)-1);
         currentDate.setDate(inputDate.substr(6,2));
     }
-    if(digitsOnly.test(inputTime)){
-
+    if(validateTime.test(inputTime)){
+        currentDate.setHours(inputTime.substr(0,2));
+        currentDate.setMinutes(inputTime.substr(2,2));
+        currentDate.setSeconds(inputTime.substr(4,2));
     }
+    currentDate.setMilliseconds(0);
+    //console.log(currentDate);
+    return currentDate.getTime();
 }
 function getNumber(inputNumber, max){
     var digitsOnly = /^\d+$/g;
+    var number = Math.floor(Math.random() * max);    
+    if(digitsOnly.test(inputNumber)){
+        if(inputNumber < max){
+            number = inputNumber;
+        }
+    }
+    //console.log(number);
+    return number;
 }
 function generateTuid(millis, serverid, counter){
-
+    var tuid = {};
+    tuid.millis = millis;
+    tuid.serverID = serverid;
+    tuid.counter = counter;
+    tuid.tuidBase = ((tuid.millis * 262144)).toString();
+    var other = (counter & 0x3FF00) | serverid;
+    bitwiseOr(tuid.tuidBase, other);
+    return tuid;
 }
 
 function tuidParser(){
@@ -72,7 +114,10 @@ function tuidGenerator(){
     var inputServerId = document.getElementById("inputServerId").value;
     var inputCounter = document.getElementById("inputCounter").value;
     var millis = getMillis(inputDate, inputTime);
-    console.log("Generate TUID Clicked "  );
+    var serverid = getNumber(inputServerId, 255);
+    var counter = getNumber(inputCounter, 1023);
+    var tuid = generateTuid(millis, serverid, counter);
+    console.log(tuid);
 };
 
 document.getElementById("generateTuid").onclick =  tuidGenerator;
